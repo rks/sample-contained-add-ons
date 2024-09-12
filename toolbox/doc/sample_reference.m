0bd6b12ef764269a920b929b2bb576274a0b4978
Randy Souza <rsouza@mathworks.com>
2024-09-12 11:39:04 -0400

    Initial files

---
+++ toolbox/doc/sample_reference.m
@@ -0,0 +1,12 @@
+%% exampleFunction
+% 
+% <<../example.png>>
+% 
+% Using |exampleFunction|:
+%
+exampleFunction(42)
+
+%% With other functions
+%
+% The output of |exampleFunction| may be passed to another function:
+plot(exampleFunction(magic(3)))
